cea626eee9afa62a1ceabd4d3fea8389a397992f
Neal Lazzari <nlazzari@gmail.com>
2018-08-26 23:07:48 -0400

    Add click handler to PostSummary to link to Post and pass state to Post component

--- src/components/PostSummary/index.js
+++ src/components/PostSummary/index.js
@@ -3,33 +3,50 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import styles from './style.module.css';
 
-export default function PostSummary({title, subtitle, image, isFeature}) {
-    const featureClass = isFeature ? styles.postSummaryFeature : '';
-    const imageStyles = {
-        backgroundImage: `url(${image})`,
-    };
-    return (
-<article className={classnames('card', styles.postSummary, featureClass)} >
-    <div className={classnames('card-content', styles.cardContent)}>
-        <div className={classnames(styles.titleContainer)}>
-            <p className={classnames('title', styles.title)}>
-                <span>
-                    {title}
-                </span>
-            </p>
-            <p className={classnames('subtitle', styles.subtitle)}>
-                <span>
-                    {subtitle}
-                </span>
-            </p>
-        </div>
-    </div>
-    <img className={classnames(styles.background)}
-         style={imageStyles}
-         alt=''     
-    />
-</article>
-    );
+export default class PostSummary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        const { history, id, title, subtitle, image, slug, author, createdAt } = this.props;
+        const state = { id, title, subtitle, image, slug, author, createdAt };
+        history.push(`/post/${id}/${slug}`, state);
+    }
+
+    render() {
+        const { title, subtitle, image, isFeature } = this.props;
+        const featureClass = isFeature ? styles.postSummaryFeature : '';
+        const imageStyles = {
+            backgroundImage: `url(${image})`,
+        };
+        return (
+            <article
+                className={classnames('card', styles.postSummary, featureClass)}
+                onClick={this.handleClick}
+            >
+                <div className={classnames('card-content', styles.cardContent)}>
+                    <div className={classnames(styles.titleContainer)}>
+                        <p className={classnames('title', styles.title)}>
+                            <span>
+                                {title}
+                            </span>
+                        </p>
+                        <p className={classnames('subtitle', styles.subtitle)}>
+                            <span>
+                                {subtitle}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+                <img className={classnames(styles.background)}
+                    style={imageStyles}
+                    alt=''
+                />
+            </article>
+        );
+    }
 }
 
 PostSummary.propTypes = {
