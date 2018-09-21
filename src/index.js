211621ad56abad314ca0e1250450f0eed0463d2f
Neal Lazzari <nlazzari@gmail.com>
2018-09-20 23:54:26 -0400

    Add Helmet to set title. Fix mobile padding issue in PostSummary

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router/immutable';
 import store, { history } from './store';
 import App from './containers/App';
+import { Helmet } from 'react-helmet';
 import ScrollToTop from './containers/ScrollToTop';
 import './index.css';
 import '../node_modules/bulma/css/bulma.min.css';
@@ -14,6 +15,9 @@ render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div>
+        <Helmet>
+          <title>react-blog</title>
+        </Helmet>
         <ScrollToTop>
           <App />
         </ScrollToTop>
